a55353b8446291d80f106bdf21775429a4a421a7
FrancoBar <fbarreneche@fi.uba.ar>
2026-04-09 20:52:19 -0300

    refactor: use struct functions and separate package in sum

--- golang/src/sum/main.go
+++ golang/src/sum/main.go
@@ -1,132 +1,84 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	"errors"
+	"log/slog"
 	"os"
 	"strconv"
 
-	"github.com/7574-sistemas-distribuidos/tp-coordinacion/common/fruititem"
-	"github.com/7574-sistemas-distribuidos/tp-coordinacion/common/messageprotocol/inner"
-	"github.com/7574-sistemas-distribuidos/tp-coordinacion/common/middleware"
+	"github.com/7574-sistemas-distribuidos/tp-coordinacion/sum"
 )
 
-const MOM_PORT = 5672
-
-type SumConfig struct {
-	Id                int
-	MomHost           string
-	InputQueue        string
-	SumAmount         int
-	SumPrefix         string
-	AggregationAmount int
-	AggregationPrefix string
-}
-
-func loadConfig() (*SumConfig, error) {
-
+func loadConfig() (sum.SumConfig, error) {
 	id, err := strconv.Atoi(os.Getenv("ID"))
 	if err != nil {
-		return nil, err
+		return sum.SumConfig{}, err
 	}
 
 	sumAmount, err := strconv.Atoi(os.Getenv("SUM_AMOUNT"))
 	if err != nil {
-		return nil, err
+		return sum.SumConfig{}, err
 	}
 
 	aggregationAmount, err := strconv.Atoi(os.Getenv("AGGREGATION_AMOUNT"))
 	if err != nil {
-		return nil, err
+		return sum.SumConfig{}, err
 	}
 
-	connectionConfig := SumConfig{
+	momPort, err := strconv.Atoi(os.Getenv("MOM_PORT"))
+	if err != nil {
+		return sum.SumConfig{}, errors.New("MOM_PORT environment variable is required and must be a number")
+	}
+
+	momHost := os.Getenv("MOM_HOST")
+	if momHost == "" {
+		return sum.SumConfig{}, errors.New("MOM_HOST environment variable is required")
+	}
+
+	inputQueue := os.Getenv("INPUT_QUEUE")
+	if inputQueue == "" {
+		return sum.SumConfig{}, errors.New("INPUT_QUEUE environment variable is required")
+	}
+
+	sumPrefix := os.Getenv("SUM_PREFIX")
+	if sumPrefix == "" {
+		return sum.SumConfig{}, errors.New("SUM_PREFIX environment variable is required")
+	}
+
+	aggregationPrefix := os.Getenv("AGGREGATION_PREFIX")
+	if aggregationPrefix == "" {
+		return sum.SumConfig{}, errors.New("AGGREGATION_PREFIX environment variable is required")
+	}
+
+	return sum.SumConfig{
 		Id:                id,
-		MomHost:           os.Getenv("MOM_HOST"),
-		InputQueue:        os.Getenv("INPUT_QUEUE"),
+		MomHost:           momHost,
+		MomPort:           momPort,
+		InputQueue:        inputQueue,
 		SumAmount:         sumAmount,
-		SumPrefix:         os.Getenv("SUM_PREFIX"),
+		SumPrefix:         sumPrefix,
 		AggregationAmount: aggregationAmount,
-		AggregationPrefix: os.Getenv("AGGREGATION_PREFIX"),
-	}
-
-	return &connectionConfig, nil
+		AggregationPrefix: aggregationPrefix,
+	}, nil
 }
 
-func handleMessage(fruitItemMap *map[string]fruititem.FruitItem, outputExchange middleware.Middleware, msg middleware.Message, ack func(), nack func()) {
-	defer ack()
-
-	fruitRecords, isEof, err := inner.DeserializeMessage(&msg)
+func run() int {
+	config, err := loadConfig()
 	if err != nil {
-		log.Println("While deserializing message", err)
-		return
+		slog.Error("While loading config", "err", err)
+		return 1
 	}
 
-	if !isEof {
-		for _, fruitRecord := range fruitRecords {
-			_, ok := (*fruitItemMap)[fruitRecord.Fruit]
-			if ok {
-				(*fruitItemMap)[fruitRecord.Fruit] = (*fruitItemMap)[fruitRecord.Fruit].Sum(fruitRecord)
-			} else {
-				(*fruitItemMap)[fruitRecord.Fruit] = fruitRecord
-			}
-		}
-
-	} else {
-		log.Println("Received end of records message")
-		for key := range *fruitItemMap {
-			fruitRecord := []fruititem.FruitItem{(*fruitItemMap)[key]}
-			message, err := inner.SerializeMessage(fruitRecord)
-			if err != nil {
-				log.Println("While serializing message", err)
-				return
-			}
-			if err := outputExchange.Send(*message); err != nil {
-				log.Println("While sending message", err)
-				return
-			}
-		}
-
-		eofMessage := []fruititem.FruitItem{}
-		message, err := inner.SerializeMessage(eofMessage)
-		if err != nil {
-			log.Println("While serializing message", err)
-			return
-		}
-		if err := outputExchange.Send(*message); err != nil {
-			log.Println("While sending message", err)
-			return
-		}
+	server, err := sum.NewSum(config)
+	if err != nil {
+		slog.Error("While initializing sum", "err", err)
+		return 1
 	}
+
+	server.Run()
+	return 0
 }
 
 func main() {
-	var fruitItemMap = map[string]fruititem.FruitItem{}
-	sumConfig, err := loadConfig()
-	if err != nil {
-		log.Println("While reading config", err)
-		return
-	}
-	connectionConfig := middleware.ConnSettings{Hostname: sumConfig.MomHost, Port: MOM_PORT}
-	inputQueue, err := middleware.CreateQueueMiddleware(sumConfig.InputQueue, connectionConfig)
-	if err != nil {
-		log.Println("While creating input queue", err)
-		return
-	}
-
-	outputExchangeRoutekeys := []string{}
-	for i := range sumConfig.AggregationAmount {
-		outputExchange := fmt.Sprintf("%s_%d", sumConfig.AggregationPrefix, i)
-		log.Println(outputExchange)
-		outputExchangeRoutekeys = append(outputExchangeRoutekeys, outputExchange)
-	}
-	outputExchange, err := middleware.CreateExchangeMiddleware(sumConfig.AggregationPrefix, outputExchangeRoutekeys, connectionConfig)
-	if err != nil {
-		log.Println("While creating input queue", err)
-		return
-	}
-
-	inputQueue.StartConsuming(func(msg middleware.Message, ack, nack func()) {
-		handleMessage(&fruitItemMap, outputExchange, msg, ack, nack)
-	})
+	os.Exit(run())
 }
